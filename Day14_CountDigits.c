777dbfe9f0d64cc3fe7ce07796d77e1467f24741
Pranamya <pranamyaks9@gmail.com>
2026-03-21 18:54:45 +0530

    Add files via upload

---
+++ Day14_CountDigits.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    int n,count=0;
+    printf("Enter the Number:");
+    scanf("%d",&n);
+    if(n==0)
+        count=1;
+    while(n!=0)
+    {
+        n=n/10;
+        count++;
+    }
+    printf("Number of digits is %d",count);
+}
