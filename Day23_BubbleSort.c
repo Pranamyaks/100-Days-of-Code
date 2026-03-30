7ada34dd06880565a2e0a9e19c2e1303240e2e69
Pranamya <pranamyaks9@gmail.com>
2026-03-30 21:28:35 +0530

    Add files via upload

---
+++ Day23_BubbleSort.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+int main()
+{
+    int n,temp,arr[10],i,j;
+    printf("Enter number of elements:");
+    scanf("%d",&n);
+    printf("Enter the array elements:");
+    for(int i=0;i<n;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    for(int i=0;i<n-1;i++)
+    {
+        for(int j=0;j<n-i-1;j++)
+        {
+            if(arr[j]>arr[j+1])
+            {
+                temp=arr[j];
+                arr[j]=arr[j+1];
+                arr[j+1]=temp;
+            }
+        }
+    }
+    printf("Sorted Array:");
+    for(int i=0;i<n;i++)
+    {
+        printf("%d\t",arr[i]);
+    }
+}
